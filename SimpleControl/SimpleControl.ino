57d813657ba9ce0a5d167de95c617e7bfb982033
MoIdriez <moidriez@gmail.com>
2016-09-04 16:06:56 +0100

    Initial Commit
    Just added all the code from the Arduino

---
+++ SimpleControl/SimpleControl.ino
@@ -0,0 +1,15 @@
+int motorLeft = 6;
+void setup() {
+  // put your setup code here, to run once:
+  //pinMode(motorLeft, OUTPUT);
+  //analogWrite(motorLeft, 255);
+}
+
+void loop() {
+  analogWrite(motorLeft, 255);
+  // put your main code here, to run repeatedly:
+//  digitalWrite(motorLeft, HIGH);
+//  delay(1000); 
+//  digitalWrite(motorLeft, LOW);
+//  delay(1000);
+}
---
+++ TankControl/TankControl.ino
@@ -0,0 +1,244 @@
+// Include for the motorshields
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+//Include for the MPU6050
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+
+// Receiving Serial Commands
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+
+// Motor control
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x64); 
+Adafruit_DCMotor *leftMotor = AFMS.getMotor(3);
+Adafruit_DCMotor *rightMotor = AFMS.getMotor(4);
+
+// MPU6050
+MPU6050 mpu;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+void setup() {
+  
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+      Wire.begin();
+      TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
+  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+      Fastwire::setup(400, true);
+  #endif
+  
+  // initialize serial:
+  Serial.begin(115200);
+  while (!Serial);
+
+  // initialize device
+  Serial.println(F("Initializing I2C devices..."));
+  mpu.initialize();
+
+  // verify connection
+  Serial.println(F("Testing device connections..."));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+  // wait for ready
+  Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+  while (Serial.available() && Serial.read()); // empty buffer
+  while (!Serial.available());                 // wait for data
+  while (Serial.available() && Serial.read()); // empty buffer again
+
+  // load and configure the DMP
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+
+  // supply your own gyro offsets here, scaled for min sensitivity
+  mpu.setXGyroOffset(0);
+  mpu.setYGyroOffset(0);
+  mpu.setZGyroOffset(-1);
+  mpu.setXAccelOffset(3);
+  mpu.setYAccelOffset(3);
+  mpu.setZAccelOffset(16377);
+
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0) {
+      // turn on the DMP, now that it's ready
+      Serial.println(F("Enabling DMP..."));
+      mpu.setDMPEnabled(true);
+
+      // enable Arduino interrupt detection
+      Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+      attachInterrupt(0, dmpDataReady, RISING);
+      mpuIntStatus = mpu.getIntStatus();
+
+      // set our DMP Ready flag so the main loop() function knows it's okay to use it
+      Serial.println(F("DMP ready! Waiting for first interrupt..."));
+      dmpReady = true;
+
+      // get expected DMP packet size for later comparison
+      packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+      // ERROR!
+      // 1 = initial memory load failed
+      // 2 = DMP configuration updates failed
+      // (if it's going to break, usually the code will be 1)
+      Serial.print(F("DMP Initialization failed (code "));
+      Serial.print(devStatus);
+      Serial.println(F(")"));
+  }
+  
+  // reserve 200 bytes for the inputString:
+  inputString.reserve(200);
+  AFMS.begin();
+  leftMotor->run(FORWARD);
+  rightMotor->run(FORWARD);
+}
+
+void loop() {
+  // print the string when a newline arrives:
+  if (stringComplete) {
+    setMotors();
+    // clear the string:
+    inputString = "";
+    stringComplete = false;
+  }
+
+  // if programming failed, don't try to do anything
+    if (!dmpReady) return;
+
+    // wait for MPU interrupt or extra packet(s) available
+    while (!mpuInterrupt && fifoCount < packetSize) {
+        // other program behavior stuff here
+        // .
+        // .
+        // .
+        // if you are really paranoid you can frequently test in between other
+        // stuff to see if mpuInterrupt is true, and if so, "break;" from the
+        // while() loop to immediately process the MPU data
+        // .
+        // .
+        // .
+    }
+
+    // reset interrupt flag and get INT_STATUS byte
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & 0x02) {
+
+      // wait for correct available data length, should be a VERY short wait
+      while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+      // read a packet from FIFO
+      mpu.getFIFOBytes(fifoBuffer, packetSize);
+      
+      // track FIFO count here in case there is > 1 packet available
+      // (this lets us immediately read more without waiting for an interrupt)
+      fifoCount -= packetSize;
+
+      Serial.print("Data:");
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      Serial.print(q.w);
+      Serial.print(",");
+      Serial.print(q.x);
+      Serial.print(",");
+      Serial.print(q.y);
+      Serial.print(",");
+      Serial.print(q.z);
+      
+      mpu.dmpGetEuler(euler, &q);
+      Serial.print(",");
+      Serial.print(euler[0] * 180/M_PI);
+      Serial.print(",");
+      Serial.print(euler[1] * 180/M_PI);
+      Serial.print(",");
+      Serial.print(euler[2] * 180/M_PI);
+
+      mpu.dmpGetGravity(&gravity, &q);
+      mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+      Serial.print(",");
+      Serial.print(ypr[0] * 180/M_PI);
+      Serial.print(",");
+      Serial.print(ypr[1] * 180/M_PI);
+      Serial.print(",");
+      Serial.print(ypr[2] * 180/M_PI);
+
+      mpu.dmpGetAccel(&aa, fifoBuffer);
+      mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+      Serial.print(",");
+      Serial.print(aaReal.x);
+      Serial.print(",");
+      Serial.print(aaReal.y);
+      Serial.print(",");
+      Serial.print(aaReal.z);
+
+      mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+      Serial.print(",");
+      Serial.print(aaWorld.x);
+      Serial.print(",");
+      Serial.print(aaWorld.y);
+      Serial.print(",");
+      Serial.println(aaWorld.z);
+    }
+    
+}
+
+/*
+  SerialEvent occurs whenever a new data comes in the
+ hardware serial RX.  This routine is run between each
+ time loop() runs, so using delay inside loop can delay
+ response.  Multiple bytes of data may be available.
+ */
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
+}
+
+void setMotors() {
+  int i = inputString.indexOf("-");
+  String lm = inputString.substring(0,i);
+  String rm = inputString.substring(i+1, inputString.length()-1);
+  leftMotor->setSpeed(lm.toInt());
+  rightMotor->setSpeed(rm.toInt());
+}
+
